f913c69ee013096660280a1b7053aff0d7d8b332
WirelessBioBot <ivan.azir.kazakov@gmail.com>
2024-05-30 11:04:20 +0300

    Chto on tvorit

--- src/main/java/com/example/projectteorver/B18.java
+++ src/main/java/com/example/projectteorver/B18.java
@@ -1,7 +1,13 @@
 package com.example.projectteorver;
 
 public class B18 extends Namber {
-    /*8. Случайная величина X распределена по нормально9
-му закону (MX = 40; DX = 200). Какова вероятность со9
-бытия {X  (30; 80)}?*/
+    B18(int seed) { super(seed); }
+    public String get() {
+        return "Случайная величина X распределена по нормально9\n" +
+                "му закону (MX = 40; DX = 200). Какова вероятность со9\n" +
+                "бытия {X  (30; 80)}?";
+    }
+    public String aswer() {
+        return "";
+    }
 }
